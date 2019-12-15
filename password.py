1e0fb79ead6a9829d219f6f4d2422bbc1499e170
wamakimaN <wamamoses@gmail.com>
2019-12-15 18:51:36 +0300

    greate an Account class for user instances

---
+++ password.py
@@ -0,0 +1,8 @@
+class Account:
+  """
+  class that generates new instances of user accounts
+  """
+  acc_list = []
+  def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
