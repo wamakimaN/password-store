a07280c17648c025c06bb850ff4e69b7cebf7314
wamakimaN <wamamoses@gmail.com>
2019-12-16 01:24:25 +0300

    function for creating new accounts

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3.6
+from password import *
+
+def create_acc(user, key):
+    """
+    function to create a new account
+    """
+    new_acc = Account(user,key)
+    return new_acc
