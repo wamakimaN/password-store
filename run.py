25e38f6bfdaed4b711daa60ebd2da4b78b268b04
wamakimaN <wamamoses@gmail.com>
2019-12-16 01:27:37 +0300

    function for creating new site credentials

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3.6
 from password import *
 
+
 def create_acc(user, key):
     """
     function to create a new account
     """
-    new_acc = Account(user,key)
+    new_acc = Account(user, key)
     return new_acc
+
+
+def create_site(sname, suser, skey):
+    """
+    function to create a new account
+    """
+    new_site = Site(sname, suser, skey)
+    return new_site
