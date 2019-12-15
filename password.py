e39fb1397cbc66b56ed342c4e6133b6013513dfc
wamakimaN <wamamoses@gmail.com>
2019-12-15 20:56:45 +0300

    add code for saving accounts

--- password.py
+++ password.py
@@ -7,3 +7,6 @@ class Account:
     def __init__(self, user_name, password):
         self.user_name = user_name
         self.password = password
+
+    def save_acc(self):
+        Account.acc_list.append(self)
